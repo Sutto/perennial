5953a8a89b302ddbb61ec811fd647ff1d25594c7
Darcy Laycock <sutto@sutto.net>
2009-08-01 13:00:25 +0800

    Refactor dir

--- lib/perennial/core_ext/misc.rb
+++ lib/perennial/core_ext/misc.rb
@@ -30,8 +30,9 @@ end
 
 module Kernel
   
+  # From http://oldrcrs.rubypal.com/rcr/show/309
   def __DIR__(offset = 0)
-    File.dirname(caller(offset + 1).first.split(":").first)
+    (/^(.+)?:\d+/ =~ caller[offset + 1]) ? File.dirname($1) : nil
   end
   
   # Shorthand for lambda
