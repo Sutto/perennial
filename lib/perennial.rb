3ea93861cef04d4326abedd0e77d06ea29e8d3b6
Darcy Laycock <sutto@sutto.net>
2009-09-04 00:12:42 +0800

    Bump gemspec, update load path hack

--- lib/perennial.rb
+++ lib/perennial.rb
@@ -1,6 +1,7 @@
 # Append the perennial lib folder onto the load path to make it
 # nicer to require perennial-related libraries.
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+perennial_dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(perennial_dir) unless $LOAD_PATH.include?(perennial_dir)
 
 require 'pathname'
 require 'perennial/core_ext'
@@ -8,7 +9,7 @@ require 'perennial/exceptions'
 
 module Perennial
   
-  VERSION = "0.2.2.2"
+  VERSION = "0.2.2.3"
   
   has_libary :dispatchable, :hookable, :loader, :logger,
              :loggable, :manifest, :settings, :argument_parser,
