48f2865adbcba6a92fd1012c28f8b9fa1688b3ab
Darcy Laycock <sutto@sutto.net>
2009-09-16 10:06:42 +0800

    Bump version / gemspec - try 2

--- lib/perennial.rb
+++ lib/perennial.rb
@@ -9,7 +9,7 @@ require 'perennial/exceptions'
 
 module Perennial
   
-  VERSION = "0.2.4.3"
+  VERSION = "0.2.4.5"
   
   has_library :dispatchable, :hookable, :loader, :logger, :nash,
               :loggable, :manifest, :settings, :argument_parser,
