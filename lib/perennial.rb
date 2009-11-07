3fa4c7518f2b23e3c2f7ef8c54976d8cd5ab1b9b
Darcy Laycock <sutto@sutto.net>
2009-11-07 22:00:36 +0800

    Bump version

--- lib/perennial.rb
+++ lib/perennial.rb
@@ -9,7 +9,7 @@ require 'perennial/exceptions'
 
 module Perennial
   
-  VERSION = [1, 0, 2, 1]
+  VERSION = [1, 1, 0]
   
   has_library :dispatchable, :hookable, :loader, :logger, :nash,
               :loggable, :manifest, :settings, :argument_parser,
